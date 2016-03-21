28a52c3e9f0cae0375d1bc04db4bcc4259831a26
jamtot <jongrammor@gmail.com>
2016-03-21 11:41:21 +0000

    Problem 20 involves finding the sum of the digits of factorial numbers.

---
+++ 20 - Factorial digit sum/factorial.py
@@ -0,0 +1,15 @@
+def fac(n):
+    if n==1:
+        return 1
+    else:
+        return n*fac(n-1)
+
+def sum_digits(n):
+    digits=[int(x) for x in str(n)]
+    return sum(digits)
+
+print fac(10)
+print fac(100)
+
+print sum_digits(fac(10))
+print sum_digits(fac(100))
