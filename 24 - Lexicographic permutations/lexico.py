77c0c42106f08789a097bd3413868e249aea57d2
jamtot <jongrammor@gmail.com>
2016-03-22 17:34:10 +0000

    Rejigged problem 24 a bit, added a function to get the amount of permutations to a set of digits.

--- 24 - Lexicographic permutations/lexico.py
+++ 24 - Lexicographic permutations/lexico.py
@@ -1,5 +1,7 @@
 import time
 
+# stopped it after 30 mins or so without an answer
+# need a new approach
 def lexicographic(nums, nth):
     found=0
     numstr=("").join(map(str,nums)).zfill(10)
@@ -12,12 +14,11 @@ def lexicographic(nums, nth):
     return current
 
 def get_next(nums):
-    # find longest sunsection that won't increase (highest perm)
-
+    # find longest subsection that won't increase (highest permutation)
     i = len(nums)-1
     while i > 0 and nums[i-1] >= nums[i]:
         i-=1
-    # now i is thet start of the subsection
+    # now i is the start of the subsection
 
     #are we at the front
     if i<=0:
@@ -37,7 +38,7 @@ def get_next(nums):
     nums[i-1] = nums[j]
     nums[j] = temp
 
-    # jiggle the numbers to reorder them from lowest to highest
+    # reverse the subsection to have from lowest to highest
     j = len(nums)-1
     while i<j:
         temp = nums[i]
@@ -51,9 +52,27 @@ def lexic(nums, nth):
     start = time.time()
     for i in xrange(1,nth):
         nums = get_next(nums)
-    
-    print "Time taken to complete: %rseconds." % (time.time() - start)
+    print "Time taken to complete: %r secs." % (time.time() - start)
     return nums
+
+def permutations_possible(nums,printit=False):
+    current=1
+    last=[1]
+    while True:
+        nums = get_next(nums)
+        if printit: print nums
+        if last==nums:
+            print "Exiting"
+            break
+        last=nums[:]
+        current+=1
+    return current
 
 if __name__=="__main__":
     print lexic([0,1,2,3,4,5,6,7,8,9], 1000000)
+    # [2, 7, 8, 3, 9, 1, 5, 4, 6, 0]
+    # 0.9446179866790771 secs
+    assert permutations_possible([0,1,2], True)==6
+    assert permutations_possible([0,1,2,3,4,5,6,7,8,9])==3628800
+    
+
