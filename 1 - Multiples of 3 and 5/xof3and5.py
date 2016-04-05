85c262be35c197bda8ab662dbfc0376dbd5254ad
jamtot <jongrammor@gmail.com>
2016-04-05 18:19:20 +0100

    Refactored the code a bit after looking back on it.

--- 1 - Multiples of 3 and 5/xof3and5.py
+++ 1 - Multiples of 3 and 5/xof3and5.py
@@ -1,23 +1,10 @@
-def sum_of_multiple(multiple, ceiling):
-    sum = 0    
-    for i in xrange(0,1000,multiple):
-        sum+=i
-    return sum
-
 def sum_of_multiples(multiple1, multiple2, ceiling):
-    sum = 0    
+    """sum = 0    
     for i in xrange(0,1000):
-        if i%multiple1 == 0 and i%multiple2 == 0:
-            sum+=i
-        elif i%multiple1 == 0: sum+=i
-        elif i%multiple2 == 0: sum+=i
-    return sum
+        if i%multiple1 == 0 or i%multiple2 == 0:
+            sum+=i"""
+    # returning after a few weeks can change so much
+    return sum([i for i in xrange(ceiling) if i%multiple1 == 0 or i%multiple2 == 0])
 
-# includes same numbers
-sum = 0
-sum+= sum_of_multiple(3,1001)
-sum+= sum_of_multiple(5,1001)
-print sum
-
-sum2 = sum_of_multiples(3,5,1000)
-print sum2
+if __name__ == "__main__":
+    print sum_of_multiples(3,5,1000)
