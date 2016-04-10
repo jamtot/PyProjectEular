98ed205a10840f08914938b4a386d5018ab3cf80
jamtot <jongrammor@gmail.com>
2016-04-10 20:06:26 +0100

    Changed the primegen into a composite/prime generator, then generated a list of doubled squares too. Then just looped through both until the number is found. Is actually quicker than I expected it to be, because it is very brute forcey.

--- 46 - Goldbach's other conjecture/goldy.py
+++ 46 - Goldbach's other conjecture/goldy.py
@@ -1,20 +1,51 @@
-def primegen():
+def primecompgen(composites = True):
     primes = [2,3]
-    yield 2
-    yield 3
+    composites = []
+    if composites == False:
+        yield 2
+        yield 3
     num = 3
     while True:
         prime = True
         num+=2
         for p in primes:
             if num%p == 0:
+                composites.append(num)
                 prime = False
             
         if prime:
             primes.append(num)
-            yield num 
+            if composites == False: yield num 
+        else: 
+            if composites: yield num, primes
+
+def sqrdubgen():
+    num = 1
+    while True:
+        sqrdub = (num*num)*2
+        yield sqrdub
+        num+=1
+
 
 if __name__ == "__main__":
-    pg = primegen()
-    for i in xrange(1000):
-        print pg.next()
+    pg = primecompgen()
+    sbgen = sqrdubgen()
+    sqrdubs = [sbgen.next()]
+    while True:
+        comp,primes = pg.next()
+        breakme = False
+        while sqrdubs[-1] < comp:
+            sqrdubs.append(sbgen.next())
+        for p in primes:
+            for sd in sqrdubs:
+                if p+sd == comp:
+                    breakme = True
+                    break
+            if breakme:
+                break
+        if breakme:
+            continue
+        break
+    print "Non-conforming number found. It is %d." % comp # it's 5777
+        
+        
